6df634f8e5eca4fe53d2922d6e83904bb23efdbc
vairam27012001 <vairammaharajan@gmail.com>
2025-05-21 15:57:04 +0530

    project added

--- program1.py
+++ program1.py
@@ -1,2 +1,7 @@
-print("hello world")
-
+number = 5432
+thousands =number//1000
+hundreds =(number%1000)//100
+tens =(number%100)//10
+ones =number%10
+sum_of_digits = thousands + hundreds + tens + ones
+print(sum_of_digits)
