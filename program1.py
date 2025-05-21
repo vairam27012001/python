2c26910784ce5e527f9f760a19d7c451a8265c77
vairam maharajan <vairammaharajan@gmail.com>
2025-05-21 16:05:15 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,3 +1,11 @@
+# Extract the thousands place by performing integer division by 1000
+# Extract the hundreds place by first performing integer division by 100, then using modulus to get the last digit
+# Extract the tens place by first performing integer division by 10, then using modulus to get the last digit
+# Extract the ones place by performing modulus 10 on the number
+# Calculate the sum of all the digits
+# Print the sum of the digits
+
+
 number = 5432
 thousands =number//1000
 hundreds =(number%1000)//100
