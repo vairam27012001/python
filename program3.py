0d2a3177a78ae4f922f92657734d2a24f3de0312
vairam27012001 <vairammaharajan@gmail.com>
2025-07-10 14:53:34 +0530

    created

--- program3.py
+++ program3.py
@@ -1,9 +1,9 @@
 a=int(input("a=",))
-if (a%3==0):
-     print("divisible by 3 ")
+if (a%3==0 and a%2==0):
+     print("it is divisible by 3 and it is an even number",a)
+elif(a%3==0 and a%2==1):
+    print ("it is divisible by 3 and it is a odd number",a)
 elif(a%2==0):
-    print ("its a even number",a)
+    print("it is not divisible by 3 and it is a even number",a)
 else:
-    print("its a odd number",a)
-      
-      
+    print("it is not divisible by 3 and it is a odd number",a)
