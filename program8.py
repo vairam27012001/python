8bd065e99c6162254c9d4601cc9876a1626e4c18
vairam27012001 <vairammaharajan@gmail.com>
2025-07-11 16:06:00 +0530

    created

--- program8.py
+++ program8.py
@@ -1,3 +1,14 @@
-def add(n1,n2):
-    return n1+n2
-a=int    
+class laptop:
+    def __init__(self):
+        self.ram=""
+        self.processor="" 
+    def display(self):
+        print("ram:",self.ram)
+        print("processor",self.processor)
+hp=laptop()
+dell=laptop()
+hp.ram="8gb"
+hp.processor="i5"
+dell.ram="16gb"
+dell.processor="i7"
+hp.display()
